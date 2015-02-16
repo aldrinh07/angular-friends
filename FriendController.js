9b814363dd3c1801ae10f49df59d84265d3fec8a
aldrinhernando <aldrinh@live.com>
2015-02-16 15:35:16 -0700

    completed

--- FriendController.js
+++ FriendController.js
@@ -1,3 +1,28 @@
+var app = angular.module('AngularFriends');
+
+app.controller('FriendCtrl', function($scope, $http){
+
+    $http.get("./friend-data.json").then(function(res){
+        $scope.friends = res.data;
+        for(var i = 0; i < $scope.friends.length; i++){
+            $scope.friends[i].friend_count = parseInt($scope.friends[i].friend_count) || 0;
+        };
+    });
+
+//    $scope.searchTerm = {};
+
+    $scope.desc = "false";
+
+    $scope.filterOpts = {
+        'Name': 'name',
+        '# Friends': 'friend_count',
+        'City': 'current_location.city',
+        'State': 'current_location.state',
+        'Country': 'current_location.country'
+    };
+
+});
+
 
 
 
